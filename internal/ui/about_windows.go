834b7d2de224a79bff6d48ac882ecdd4df62a4f2
Ghosted Owl <170436570+GhostedOwl@users.noreply.github.com>
2026-07-21 09:38:07 +0300

    fix: about_windows — hardcode version/date, no dep on webui

--- internal/ui/about_windows.go
+++ internal/ui/about_windows.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"syscall"
 
-	"github.com/JastRedPanda/Nimbus/internal/webui"
 	"github.com/lxn/win"
+)
+
+const (
+	buildVersion = "0.1.4"
+	buildDate    = "07.2026"
 )
 
 func ShowAbout() {
 	title := syscall.StringToUTF16("About Nimbux")
-	msg := syscall.StringToUTF16(fmt.Sprintf("Nimbux\nМультиплатформний інформер погоди.\n\n%s\nv%s", webui.BuildDate, webui.BuildVersion))
+	msg := syscall.StringToUTF16(fmt.Sprintf("Nimbux\nМультиплатформний інформер погоди.\n\n%s\nv%s", buildDate, buildVersion))
 	win.MessageBox(0, &msg[0], &title[0], win.MB_OK|win.MB_ICONINFORMATION)
 }
